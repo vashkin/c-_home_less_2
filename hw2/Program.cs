9c7f9034aa0c51e0c3e514dad9c3e8840c9cbfc4
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-14 01:52:59 +0400

    homework add 3

---
+++ hw2/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("Введите число дня недели\n");
+ Console.WriteLine("от 1 до 7:");
+ int days = Convert.ToInt32( Console.ReadLine()!);
+
+
+ string [] ar = { "","понедельник", "вторник", "среду", "четверг", "пятницу", "субботу", "воскресенье"};
+
+
+ if (days>=6 ){
+    Console.WriteLine("В "+ar[days] + " отдыхаем");
+ }else if(days != 0) {
+    Console.WriteLine("В "+ar[days] + " работаем");
+ }
