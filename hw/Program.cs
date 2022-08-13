efc5bcdb17de0d0a87bbb344c0d87fb1908eea5b
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-14 00:55:31 +0400

    checking conditions

--- hw/Program.cs
+++ hw/Program.cs
@@ -2,9 +2,21 @@
 // C - цифра в числе по счёту
 // N - вводное число
 {
-string str = ""+N;
-char [] ar = str.ToArray<char>();
-return (int)Char.GetNumericValue(ar[C]);
+    int i = 1;
+    int mas = 1;
+    do{
+        mas= mas*10;
+        i++;
+    }while(i<=C-1);
+     if (mas>=N){
+                 Console.WriteLine( i + " цифры нет");
+            return 0;
+    }else{
+                string str = ""+N;
+                char [] ar = str.ToArray<char>();
+                return (int)Char.GetNumericValue(ar[C-1]);
+    }  
 }
 
-Console.WriteLine("" + NumberCont(1,12345678));
+
+Console.WriteLine("" + NumberCont(9,12345678));
